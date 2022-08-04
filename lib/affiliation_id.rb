215dae9cd0e7ffa5fe470d0a7479f22770a4276f
siklodi-mariusz <9211589+siklodi-mariusz@users.noreply.github.com>
2022-08-04 18:34:35 +0300

    First commit

---
+++ lib/affiliation_id.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'affiliation_id/version'
+
+module AffiliationId
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/affiliation_id_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AffiliationId do
+  it 'has a version number' do
+    expect(AffiliationId::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end

ab285930c4632621ed82e34a7090b6ea9c234acd
siklodi-mariusz <9211589+siklodi-mariusz@users.noreply.github.com>
2022-08-05 16:41:03 +0300

    Implement AffiliationId public API to get, set and renew affiliation id

--- lib/affiliation_id.rb
+++ lib/affiliation_id.rb
@@ -1,8 +1,40 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 require_relative 'affiliation_id/version'
 
-module AffiliationId
-  class Error < StandardError; end
-  # Your code goes here...
+module AffiliationId # :nodoc:
+  THREAD_KEY = 'AFFILIATION_ID'
+
+  class << self
+    #
+    # Returns the current Affiliation ID
+    #
+    # @return [String] Uniq Affiliation ID
+    #
+    def current_id
+      Thread.current[THREAD_KEY] ||= SecureRandom.uuid
+    end
+
+    #
+    # Sets a new ID to be used as Affiliation ID
+    #
+    # @param [String] value of Affilication ID
+    #
+    # @return [String] Affiliation ID
+    #
+    def current_id=(value)
+      Thread.current[THREAD_KEY] = value
+    end
+
+    #
+    # Renew the current Affiliation ID with a new one
+    #
+    # @return [String] Affiliation ID
+    #
+    def renew_current_id!
+      Thread.current[THREAD_KEY] = SecureRandom.uuid
+    end
+  end
 end
--- spec/affiliation_id_spec.rb
+++ spec/affiliation_id_spec.rb
@@ -5,7 +5,39 @@ RSpec.describe AffiliationId do
     expect(AffiliationId::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.current_id' do
+    subject { described_class.current_id }
+
+    it { is_expected.to be }
+
+    it 'returns the same id on multiple calls' do
+      expect(Array.new(5) { described_class.current_id }.uniq).to be_one
+    end
+  end
+
+  describe '.current_id=' do
+    let(:new_id) { SecureRandom.uuid }
+    subject { described_class.current_id = new_id }
+
+    it 'changes current_id' do
+      expect { subject }.to change { described_class.current_id }.to(new_id)
+    end
+  end
+
+  describe '.renew_current_id!' do
+    subject { described_class.renew_current_id! }
+
+    it 'generates current_id' do
+      expect(subject).to be
+    end
+
+    it 'generates new current_id on each call' do
+      expect(Array.new(5) { described_class.renew_current_id! }.uniq.length).to eq(5)
+    end
+
+    it 'stores the new current_id for future calls' do
+      renewed = subject
+      expect(renewed).to eq(described_class.current_id)
+    end
   end
 end
